38150dae0034fefcd9a989e86748024a6c951cd5
Kaspar Jõeveer <kjoeveer@gmail.com>
2025-03-03 08:57:44 +0200

    Remove IconHighScore import

--- src/components/GameBar.vue
+++ src/components/GameBar.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import type { Game } from '@/types/game';
-import GameBarStat from './GameBarStat.vue';
-import IconHealth from './icons/IconHealth.vue';
-import IconGold from './icons/IconGold.vue';
-import IconTurn from './icons/IconTurn.vue';
-import IconLevel from './icons/IconLevel.vue';
-import IconScore from './icons/IconScore.vue';
-import IconHighScore from './icons/IconHighScore.vue';
+import type { Game } from '@/types/game'
+import GameBarStat from './GameBarStat.vue'
+import IconHealth from './icons/IconHealth.vue'
+import IconGold from './icons/IconGold.vue'
+import IconTurn from './icons/IconTurn.vue'
+import IconLevel from './icons/IconLevel.vue'
+import IconScore from './icons/IconScore.vue'
 
 defineProps<{
   game: Game | null
@@ -17,23 +16,23 @@ defineProps<{
   <div class="soft-red stat-bar">
     <template v-if="game">
       <GameBarStat labelText="Lives">
-        <IconHealth class="stat-icon"/>
+        <IconHealth class="stat-icon" />
         <p>{{ game.lives }}</p>
       </GameBarStat>
       <GameBarStat labelText="Gold">
-        <IconGold class="stat-icon"/>
+        <IconGold class="stat-icon" />
         <p>{{ game.gold }}</p>
       </GameBarStat>
       <GameBarStat labelText="Turn">
-        <IconTurn class="stat-icon"/>
+        <IconTurn class="stat-icon" />
         <p>{{ game.turn }}</p>
       </GameBarStat>
       <GameBarStat labelText="Level">
-        <IconLevel class="stat-icon"/>
+        <IconLevel class="stat-icon" />
         <p>{{ game.level }}</p>
       </GameBarStat>
       <GameBarStat labelText="Score">
-        <IconScore class="stat-icon"/>
+        <IconScore class="stat-icon" />
         <p>{{ game.score }}</p>
       </GameBarStat>
     </template>
